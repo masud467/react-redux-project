b687148d73f03925421cf049a60def7035ffda53
masud467 <masudpfrnt@gmail.com>
2025-08-13 15:12:36 +0600

    added all functionality with redux.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import { useSelector } from "react-redux";
 import Todo from "./Todo";
 
 const TodoList = () => {
+  const todos = useSelector((state) => state?.todos);
+  const filters = useSelector((state) => state.filters);
+
+  const filterByStatus = (todo) => {
+    const { status } = filters;
+    switch (status) {
+      case "Complete":
+        return todo.completed;
+      case "Incomplete":
+        return !todo.completed;
+      default:
+        return true;
+    }
+  };
+
+  const filterByColors = (todo) => {
+    const { colors } = filters;
+    if (colors.length > 0) {
+      return colors.includes(todo?.color);
+    }
+    return true;
+  };
   return (
-    <div class="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-      <Todo />
+    <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
+      {todos
+        .filter(filterByStatus)
+        .filter(filterByColors)
+        .map((todo) => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
     </div>
   );
 };

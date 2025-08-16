db6005e877c1e52fc1ed24bb81b96ef6885b63d2
masud467 <masudpfrnt@gmail.com>
2025-08-16 12:19:16 +0600

    Remove static todos data from initialState and I will take data from api.

--- src/redux/todos/initialState.js
+++ src/redux/todos/initialState.js
@@ -1,13 +1,1 @@
-export const initialState = [
-  {
-    id: 1,
-    text: "Learn React 19",
-    completed: true,
-  },
-  {
-    id: 2,
-    text: "Learn React Redux",
-    completed: false,
-    color: "red",
-  },
-];
+export const initialState = [];
